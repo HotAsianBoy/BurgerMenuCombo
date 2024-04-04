fab6103ec54f1b2bae3112884e83ff5e5d0269c9
HotAsianBoy <wongg@middleton.school.nz>
2024-04-05 11:24:19 +1300

    burger menu combo main loop
    burger menu combo main loop finished

---
+++ main_v3.py
@@ -0,0 +1,30 @@
+"""Main v3
+Added extra aesthetics to ensure an effective and simple program
+for the user"""
+
+
+# Main loop
+while True:
+    print("\n*** Welcome! What would you like to do today? ***\nOptions:")
+    print("****************************************")
+    print("1 - Add a new combo meal")
+    print("2 - Search for an existing combo meal")
+    print("3 - Delete a combo meal")
+    print("4 - Display the full menu")
+    print("5 - Exit")
+    print("****************************************")
+    choice = input("Please choose an option: ")
+
+    if choice == '1':
+        add_combo()
+    elif choice == '2':
+        search_combo()
+    elif choice == '3':
+        delete_combo()
+    elif choice == '4':
+        print_menu()
+    elif choice == '5':
+        print("Exiting program.")
+        break
+    else:
+        print("Invalid option. Please try again.")
